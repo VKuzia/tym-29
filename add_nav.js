2c785959d6857ccbeee0284a5241f5d1199151b1
VKuzia <v.kuzia.mail@gmail.com>
2020-11-28 00:38:14 +0300

    Add main pages
    Добавлены основные страницы, налажено общение между ними с
    использованием панели навигации.

---
+++ add_nav.js
@@ -0,0 +1,19 @@
+document.write(`
+<nav>
+  <div class="navbar">
+    <a href="index.html">Главная</a>
+    <a href="29.html">29ка</a>
+    <div class="dropdown">
+      <button class="dropbtn">Роли</button>
+      <div class="dropdown-content">
+        <a href="roles/reporter/reporter.html">Докладчик</a>
+        <a href="roles/opponent/opponent.html">Оппонент</a>
+        <a href="roles/reviewer/reviewer.html">Рецензент</a>
+        <a href="roles/observer/observer.html">Наблюдатель</a>
+        <hr>
+        <a href="roles/roles.html">О ролях</a>
+      </div>
+    </div> 
+  </div>
+</nav>
+`);
