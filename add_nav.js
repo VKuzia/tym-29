5931fd001eb5615a7fc249b10af5ca45400233b6
VKuzia <v.kuzia.mail@gmail.com>
2020-11-28 15:11:16 +0300

    Add perefirial pages
    Добавлена пара полезных страничек. Отредактированы страницы ролей

--- add_nav.js
+++ add_nav.js
@@ -1,19 +1,31 @@
 document.write(`
 <nav>
-  <div class="navbar">
-    <a href="index.html">Главная</a>
-    <a href="29.html">29ка</a>
-    <div class="dropdown">
-      <button class="dropbtn">Роли</button>
-      <div class="dropdown-content">
-        <a href="roles/reporter/reporter.html">Докладчик</a>
-        <a href="roles/opponent/opponent.html">Оппонент</a>
-        <a href="roles/reviewer/reviewer.html">Рецензент</a>
-        <a href="roles/observer/observer.html">Наблюдатель</a>
-        <hr>
-        <a href="roles/roles.html">О ролях</a>
+   <div class="navbar">
+      <a href="index.html">Главная</a>
+      <a href="tournaments.html">Турниры</a>
+      <div class="dropdown">
+         <button class="dropbtn">Подготовка</button>
+         <div class="dropdown-content">
+            <div>
+               <a href="">Оформление материалов</a>
+               <a href="">Нулевой тур</a>
+               <a href="">Перед боем</a>
+               <a href="">Бой</a>
+            </div>
+         </div>
       </div>
-    </div> 
-  </div>
+      <div class="dropdown">
+         <button class="dropbtn">Роли</button>
+         <div class="dropdown-content">
+            <a href="roles/reporter/reporter.html">Докладчик</a>
+            <a href="roles/opponent/opponent.html">Оппонент</a>
+            <a href="roles/reviewer/reviewer.html">Рецензент</a>
+            <a href="roles/observer/observer.html">Наблюдатель</a>
+            <hr>
+            <a href="roles/roles.html">О ролях</a>
+         </div>
+      </div>
+      <a href="29.html">Гимназия №29</a>
+   </div>
 </nav>
 `);
