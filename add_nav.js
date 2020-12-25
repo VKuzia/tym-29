68b112d2a0b9e6e0ad58030e937b2865c21a5f6c
VKuzia <v.kuzia.mail@gmail.com>
2020-12-25 22:38:14 +0300

    Add style fixes and encoding
    Добавлены .gitignore, кодировка в head и слегка структурирован styles.css

--- add_nav.js
+++ add_nav.js
@@ -1,32 +1,37 @@
 document.write(`
-<nav>
-   <div class="navbar">
-      <a href="index.html">Главная</a>
-      <a href="tournaments.html">Турниры</a>
-      <div class="dropdown">
-         <button class="dropbtn">Подготовка</button>
-         <div class="dropdown-content">
-            <div>
-               <a href="preparation/teamleads.html">Руководители</a>
-               <a href="preparation/written_materials.html">Оформление материалов</a>
-               <a href="preparation/quiz.html">Нулевой тур</a>
-               <a href="preparation/before_round.html">Перед боем</a>
-               <a href="preparation/round.html">Бой</a>
-            </div>
-         </div>
-      </div>
-      <div class="dropdown">
-         <button class="dropbtn">Роли</button>
-         <div class="dropdown-content">
-            <a href="roles/reporter/reporter.html">Докладчик</a>
-            <a href="roles/opponent/opponent.html">Оппонент</a>
-            <a href="roles/reviewer/reviewer.html">Рецензент</a>
-            <a href="roles/observer/observer.html">Наблюдатель</a>
-            <hr>
-            <a href="roles/roles.html">О ролях</a>
-         </div>
-      </div>
-      <a href="29.html">Гимназия №29</a>
-   </div>
+<nav class="navbar">
+  <ul style="list-style-type:none;">
+     <li><a href="index.html">Главная</a></li>
+     <li><a href="tournaments.html">Турниры</a></li>
+     <li>
+        <div class="dropdown">
+           <a>Подготовка</a>
+           <div class="dropdown-content">
+              <ul style="list-style-type:none;">
+                 <li><a href="preparation/teamleads.html">Руководители</a></li>
+                 <li><a href="preparation/written_materials.html">Оформление материалов</a></li>
+                 <li><a href="preparation/quiz.html">Нулевой тур</a></li>
+                 <li><a href="preparation/before_round.html">Перед боем</a></li>
+                 <li><a href="preparation/round.html">Бой</a></li>
+              </ul>
+           </div>
+        </div>
+     </li>
+     <li>
+        <div class="dropdown">
+           <a>Роли</a>
+           <div class="dropdown-content">
+              <ul style="list-style-type:none;">
+                 <li><a href="roles/reporter/reporter.html">Докладчик</a></li>
+                 <li><a href="roles/opponent/opponent.html">Оппонент</a></li>
+                 <li><a href="roles/reviewer/reviewer.html">Рецензент</a></li>
+                 <li><a href="roles/observer/observer.html">Наблюдатель</a></li>
+                 <hr>
+                 <li><a href="roles/roles.html">О ролях</a></li>
+              </ul>
+           </div>
+        </div>
+     </li>
+     <li><a href="29.html">Гимназия №29</a></li>
 </nav>
 `);
