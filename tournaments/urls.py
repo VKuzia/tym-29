47f1f3ac978fe5fce9816aa08e87c5762ecfd8d0
VKuzia <v.kuzia.mail@gmail.com>
2021-01-16 01:09:39 +0300

    Set up basic pages
    Применён ряд изменений для основных страниц сайта.
    Добавлены имена к основным страницам, теперь на них проще ссылаться из недр html.
    Удалены ненужные комментарии во views.py.
    Удалён более ненужный add_nav.js

--- tournaments/urls.py
+++ tournaments/urls.py
@@ -1,8 +1,11 @@
-from .views import home
+from .views import home_page, tournaments_page, twenty_nine_page
 from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home),
+    path('', home_page, name="home"),
+    path('tournaments/', tournaments_page, name="tournaments"),
+    path('29/', twenty_nine_page, name="29"),
+    path('tournaments/', tournaments_page, name="tournaments"),
 ]
--- tournaments/views.py
+++ tournaments/views.py
@@ -1,14 +1,19 @@
 import os
 from django.shortcuts import render
 from django.conf import settings
-from django.conf.urls.static import static
 
-def home(request):
-    # path = STATIC_URL + "assets/images/slideshow"
-    # path = static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+def home_page(request):
     path = "." + settings.STATIC_URL + "assets/images/slideshow/"
     print(path)
     slideshow_images = os.listdir(path)
     print(slideshow_images)
     return render(request, "index.html", {"slideshow_images": slideshow_images})
-    # return render(request, "index.html")
+
+
+def tournaments_page(request):
+    return render(request, "tournaments.html")
+
+
+def twenty_nine_page(request):
+    return render(request, "29.html")
