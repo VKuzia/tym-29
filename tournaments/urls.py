29ddf9f1266427fadb2c45fb9a1a37d7142397ca
VKuzia <v.kuzia.mail@gmail.com>
2021-01-16 01:48:15 +0300

    Reorganize links logic
    Переделаны ссылки между страницами согласно текущей логике
    (с использованием имён страниц).
    Для статьи о письменных материалах переделаны ссылки на статичные данные.

--- tournaments/urls.py
+++ tournaments/urls.py
@@ -1,11 +1,23 @@
-from .views import home_page, tournaments_page, twenty_nine_page
+from tournaments import views
 from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home_page, name="home"),
-    path('tournaments/', tournaments_page, name="tournaments"),
-    path('29/', twenty_nine_page, name="29"),
-    path('tournaments/', tournaments_page, name="tournaments"),
+
+    path('', views.home_page, name="home"),
+    path('tournaments/', views.tournaments_page, name="tournaments"),
+    path('29/', views.twenty_nine_page, name="29"),
+
+    path('preparation/before_round', views.before_round_page, name="before_round"),
+    path('preparation/quiz', views.quiz_page, name="quiz"),
+    path('preparation/round', views.round_page, name="round"),
+    path('preparation/teamleads', views.teamleads_page, name="teamleads"),
+    path('preparation/written_materials', views.written_materials_page, name="written_materials"),
+
+    path('roles/observer', views.observer_page, name="observer"),
+    path('roles/opponent', views.opponent_page, name="opponent"),
+    path('roles/reporter', views.reporter_page, name="reporter"),
+    path('roles/reviewer', views.reviewer_page, name="reviewer"),
+    path('roles/roles', views.roles_page, name="roles"),
 ]
--- tournaments/views.py
+++ tournaments/views.py
@@ -17,3 +17,43 @@ def tournaments_page(request):
 
 def twenty_nine_page(request):
     return render(request, "29.html")
+
+
+def before_round_page(request):
+    return render(request, "preparation/before_round.html")
+
+
+def quiz_page(request):
+    return render(request, "preparation/quiz.html")
+
+
+def round_page(request):
+    return render(request, "preparation/round.html")
+
+
+def teamleads_page(request):
+    return render(request, "preparation/teamleads.html")
+
+
+def written_materials_page(request):
+    return render(request, "preparation/written_materials.html")
+
+
+def roles_page(request):
+    return render(request, "roles/roles.html")
+
+
+def observer_page(request):
+    return render(request, "roles/observer.html")
+
+
+def opponent_page(request):
+    return render(request, "roles/opponent.html")
+
+
+def reporter_page(request):
+    return render(request, "roles/reporter.html")
+
+
+def reviewer_page(request):
+    return render(request, "roles/reviewer.html")
