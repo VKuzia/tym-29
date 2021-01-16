0f7c684cae185c49c68c779501e167a216e8bc76
VKuzia <v.kuzia.mail@gmail.com>
2021-01-16 11:39:08 +0300

    Add whitenoise dependency
    Добавлен автоматический обработчик статических файлов WhiteNoise.

--- tournaments/settings.py
+++ tournaments/settings.py
@@ -34,9 +34,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
-
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -44,6 +44,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
 
 ROOT_URLCONF = 'tournaments.urls'
 
